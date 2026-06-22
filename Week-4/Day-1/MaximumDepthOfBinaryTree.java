4a751007fb7bbd4975d548a9f250123bbb5b7af7
Mayank Chaudhary <mayank10121976@gmail.com>
2026-06-22 08:23:43 +0530

    Week-4 Day-1 solutions

---
+++ Week-4/Day-1/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int L=maxDepth(root.left);
+        int R=maxDepth(root.right);
+        return 1+Math.max(L,R);
+    }
+}
